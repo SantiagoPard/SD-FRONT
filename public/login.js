2a544d4a0a042edb7e332a4bba04bac8d371879b
SantiagoPard <lspardo@uniboyaca.edu.co>
2024-12-02 12:19:26 -0500

    prueba

--- public/login.js
+++ public/login.js
@@ -1,25 +1,25 @@
 const mensajeError = document.getElementsByClassName("error")[0];
 
 document.getElementById("login-form").addEventListener("submit",async (e)=>{
-    e.preventDefault();
-    console.log(e.target.children.user.value);
+    // e.preventDefault();
+    // console.log(e.target.children.user.value);
    
 
-    const res = await fetch("http://localhost:4000/api/login",
-        {
-        method:"POST",
-        headers:{
-            "Content-Type" : "application/json"
-        },
-        body: JSON.stringify({
-            user: e.target.children.user.value
-        })
-    })
+    // const res = await fetch("http://localhost:4000/api/login",
+    //     {
+    //     method:"POST",
+    //     headers:{
+    //         "Content-Type" : "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         user: e.target.children.user.value
+    //     })
+    // })
 
-    if(!res.ok)return mensajeError.classList.toggle("escondido",false);
-    const resJson = await res.json();
-    if(resJson.redirect){
-        window.location.href = resJson.redirect
-    }
+    // if(!res.ok)return mensajeError.classList.toggle("escondido",false);
+    // const resJson = await res.json();
+    // if(resJson.redirect){
+    //     window.location.href = resJson.redirect
+    // }
     
 })
